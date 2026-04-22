1d468790c1440d6a774ec5d7193951a583ee378b
GeSi <sdropoulos@gmail.com>
2026-04-22 19:43:04 +0300

    Mpempes first ever commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class  Main {
+    public static void main(String[] args) {
+
+
+
+        int date = 3 ;
+        int year = 2026 ;
+        String declarationDate = "Σήμερα " + date + " του μήνα " + year ;
+
+        String greatingName = "kalimera me lene Konstantina";
+        int number1 = 1 ;
+        int number2 = 2;
+        int summary = 0;
+
+        summary = number1 + number2 ;
+
+        System.out.println("Καλημέρα !\n\t".repeat(3) );
+        System.out.println(declarationDate);
+        System.out.println(summary);
+        System.out.println(greatingName);
+
+    }
+}
